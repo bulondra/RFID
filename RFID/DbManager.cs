3b8c46372bcb90905b36d1d5a1d47ae25d311189
bulondra <dodisek17@gmail.com>
2023-04-12 21:53:50 +0200

    Added GetExactCode method to DbManager.cs

--- RFID/DbManager.cs
+++ RFID/DbManager.cs
@@ -18,7 +18,17 @@ namespace RFID
         public void Connect() => _connection.Open(); // Open connection
 
         public void Disconnect() => _connection.Close(); // Close connection
-        
 
+        public CodeModel GetExactCode(string code)
+        {
+            string statement = "SELECT * FROM codes WHERE code = '" + code + "';"; // Create statement
+            
+            var command = new SQLiteCommand(statement, _connection); // Create command
+            
+            SQLiteDataReader reader = command.ExecuteReader(); // Create reader
+
+            return reader.Read() ? new CodeModel { code_id = reader.GetInt32(0), code = reader.GetString(1), valid = reader.GetBoolean(2) } : null; // Return CodeModel or null
+        }
+        
     }
 }
