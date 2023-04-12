bbc7bd5b881dc1c9470f27fe5ffc951230ef0c0b
bulondra <dodisek17@gmail.com>
2023-04-12 22:33:33 +0200

    Added testing mode feature

--- RFID/Form1.cs
+++ RFID/Form1.cs
@@ -5,14 +5,14 @@ namespace RFID
 {
     public partial class Form1 : Form
     {
-        // Declaration of managers & cycle
+        // Declaration of managers, cycle & testing var
         private AuthManager _authManager;
         private EventManager _eventManager;
         private PortManager _portManager;
         private DbManager _dbManager;
         private TestManager _testManager;
         private System.Timers.Timer mainCycle;
-
+        private bool _testing;
 
         public Form1()
         {
@@ -23,12 +23,19 @@ namespace RFID
             _dbManager = new DbManager(this);
             _testManager = new TestManager(this);
 
+            _testing = true; // Testing variable
+
             InitializeComponent();
 
-            GetPortManager().OpenPort(); // Open port
+            if (!_testing) GetPortManager().OpenPort(); // Open port
             
             GetDbManager().Init(); // Init database
+            GetDbManager().Connect(); // Connect database
 
+            GetTestManager().TestAuthorization(GetTestManager().GetTestData()[3]); // Testing
+
+            if (_testing) return;
+            
             // Initialize & start main cycle
             mainCycle = new System.Timers.Timer();
             mainCycle.Elapsed += new ElapsedEventHandler(GetEventManager().MainCycle_Elapsed);
