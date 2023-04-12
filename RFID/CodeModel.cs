aa069293c7078a41dcc6d7f118e8d066d9623507
bulondra <dodisek17@gmail.com>
2023-04-12 21:35:39 +0200

    Added CodeModel.cs

---
+++ RFID/CodeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RFID
+{
+    public class CodeModel
+    {
+        private int code_id { get; set; }
+        private string code { get; set; }
+        private bool valid { get; set; }
+    }
+}
