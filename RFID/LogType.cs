c4b09f7e5794c774dd4797639f701ba5efb7c2d7
bulondra <dodisek17@gmail.com>
2023-04-12 21:58:15 +0200

    Added log types

---
+++ RFID/LogType.cs
@@ -0,0 +1,12 @@
+﻿namespace RFID
+{
+    public enum LogType
+    {
+        UNSUCCESSFUL_ACCESS,
+        SUCCESSFUL_ACCESS,
+        USER_MOD,
+        CODE_MOD,
+        PLACE_MOD,
+        ACCESS_MOD
+    }
+}
