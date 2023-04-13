d1dc99a35cdabe74049220b03c9681c2673a4f98
bulondra <dodisek17@gmail.com>
2023-04-13 09:37:11 +0200

    Added AccessModel.cs

---
+++ RFID/AccessModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RFID
+{
+    public class AccessModel
+    {
+        public int access_id { get; set; }
+        public string code { get; set; }
+        public string place { get; set; }
+        public int available_from { get; set; }
+        public int available_until { get; set; }
+    }
+}
