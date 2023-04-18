b877aa5274c1af8d3880dff2439f86db4c0b751e
bulondra <dodisek17@gmail.com>
2023-04-18 19:39:08 +0200

    Added Administration.cs

---
+++ RFID/Administration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RFID
+{
+    public partial class Administration : Form
+    {
+        public Administration(Form1 _instance)
+        {
+            InitializeComponent();
+
+            mainCycle.Start();
+            var logs = _instance.GetDbManager().GetLogs();
+            int usersCount = _instance.GetDbManager().GetUsersCount();
+            currentUsersText.Text = usersCount.ToString();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            currentDateText.Text = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
+        }
+    }
+}
