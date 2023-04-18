8bce86483078a321d0c471046bbcae60e0c5ad74
bulondra <dodisek17@gmail.com>
2023-04-18 19:38:49 +0200

    Added DbLogModel.cs

---
+++ RFID/DbLogModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFID
+{
+    public class DbLogModel
+    {
+        public int log_id { get; set; }
+        public string code { get; set; }
+        public string place { get; set; }
+        public string action { get; set; }
+        public long created_at { get; set; }
+    }
+}
